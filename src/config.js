6ff70d3095c9f78c49ca68d2b8a397d17d3c4ad4
MJ <mjstthomas9516@gmail.com>
2020-08-29 11:44:06 -0700

    fixed endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://secure-headland-88755.herokuapp.com/api',
   }
 
   // 'https://secure-headland-88755.herokuapp.com/api'
